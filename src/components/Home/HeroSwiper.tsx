81cfafca08a5d0e1e71deeb0dd30fdfbf8aeec45
Neha <nehaboora49@gmail.com>
2025-10-31 16:47:51 +0530

    set the nexxt js and get the elements of the project

---
+++ src/components/Home/HeroSwiper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroSwiper = () => {
+  return (
+    <div>HeroSwiper</div>
+  )
+}
+
+export default HeroSwiper
---
+++ src/components/common/Description.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <h3>Description</h3>
+  )
+}
+
+export default Description
