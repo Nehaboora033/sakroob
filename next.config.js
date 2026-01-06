ac38ae85c499fe7859aeda87bac163abc788fea9
Neha Boora <nehaboora49@gmail.com>
2026-01-06 16:52:27 +0530

    completed

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 };
 
-module.exports = nextConfig;
-
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-  
-//     images: {
-//       domains: ['lh3.googleusercontent.com'],
-//     },
-//   };
-
-
-// export default nextConfig;
+export default nextConfig;
